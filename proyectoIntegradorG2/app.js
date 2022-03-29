87ece8bb08ab3639e284a5f0eff7ddc664f2d64a
Jana Fernandez <jcfernandez2@udesa.edu.ar>
2022-03-29 15:27:35 -0300

    breve:db

--- proyectoIntegradorG2/app.js
+++ proyectoIntegradorG2/app.js
@@ -7,6 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+/*
+const  = require('.');
+const  = require('.');
+*/
 var app = express();
 
 // view engine setup
@@ -21,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+/* app.use();
+app.use();
+ app.use();*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
