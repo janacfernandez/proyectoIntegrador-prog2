58ec941b9e448601d9b7dd0b2b99148715502f52
Jana Fernandez <jcfernandez2@udesa.edu.ar>
2022-04-08 19:41:46 -0300

    headerLogueado y product-detail

--- proyectoIntegradorG2/controllers/productController.js
+++ proyectoIntegradorG2/controllers/productController.js
@@ -1,21 +1,11 @@
+
 const data = require('../db/data');
 
 const controller = {
 
-detail:  (req, res) => {
-       return res.render('product-detail', {
-          listaAutos : data.productos,
-          id: req.params.id - 1,
-         comment : data.comentarios,
-          
-       })
+detail:  (req, res) => res.render('product-detail', { listaAutos : data.productos, id: req.params.id - 1, comment : data.comentarios, }), 
 
-    },
-    add: (req, res) => {
-      return res.render('product-add'), {
-   
-      }
-    }, 
+    add: (req, res) => res.render('product-add', { nombreUsuario: data.usuario.usuario, }) 
 
 }; 
 
--- proyectoIntegradorG2/controllers/userController.js
+++ proyectoIntegradorG2/controllers/userController.js
@@ -2,27 +2,14 @@ const users = require('../db/data');
 
 
 const controller = {
-    login: (req,res) => {
-        return res.render('login')
-    }, 
-    register: (req,res) => {
-        return res.render('register')
-    }, 
-    profileEdit: (req,res) => {
-        return res.render('profile-edit',{
-            listaAutos : users.productos,
-            nombreUsuario : users.usuario.usuario,
-            img : users.usuario.foto,
-           }) 
-    }, 
-   profile: (req, res) => { 
-       return res.render('profile',{
-        listaAutos : users.productos,
-        nombreUsuario : users.usuario.usuario,
-        img : users.usuario.foto,
-       }) 
+    login: (req,res) => res.render('login'),
+     
+    register: (req,res) =>  res.render('register'),
 
-   },
+    profileEdit: (req,res) => res.render('profile-edit',{ listaAutos : users.productos, nombreUsuario : users.usuario.usuario, img : users.usuario.foto,
+           }), 
+
+   profile: (req, res) =>  res.render('profile',{ listaAutos : users.productos, img : users.usuario.foto, nombreUsuario: users.usuario.usuario }),
 
 };
 
