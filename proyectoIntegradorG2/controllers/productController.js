3d4e76616609963f024bc518685a63ceaa83c260
Jana Fernandez <jcfernandez2@udesa.edu.ar>
2022-06-10 14:37:18 -0300

    primer avance add y detail

--- proyectoIntegradorG2/controllers/productController.js
+++ proyectoIntegradorG2/controllers/productController.js
@@ -1,4 +1,7 @@
 const data = require('../db/data');
+const db = require("../database/models");
+const product = db.Product;
+const bcrypt = require('bcryptjs');
 
 const controller = {
     detail:  (req, res) => res.render('product-detail', { 
@@ -6,8 +9,70 @@ const controller = {
         id: req.params.id - 1, 
         comment: data.comentarios}),
 
-    add: (req, res) => res.render('product-add', {
-        nombreUsuario: data.usuario.usuario})
+    add: (req, res) =>  res.render('product-add'),
+
+    procesarAdd: (req, res) => {
+        let info = req.body;
+        let producto = {
+            img: '',
+            nombre: info.nombre,
+            descripcion: info.descripcion,
+            anio: info.anio,
+            created_at: new DATE(),
+            updated_at: new DATE()
+        }
+        product.create(producto)
+        .then((result) => {
+            return res.redirect("/")
+          }).catch((err) => {
+            return res.send("Hay un error" + err)
+          });
+        }, 
+
+    edit: (req, res) => {
+        product.findByPK(id)
+        .then((result) => {
+           
+            let producto = {
+                img: '',
+                nombre: info.nombre,
+                descripcion: info.descripcion,
+                anio: info.anio,
+                created_at: new DATE(),
+                updated_at: new DATE()
+            }
+            return res.render(),( 'productEdit',{ product : producto})
+        })
+    },
+
+    updateAdd : (req, res) => {
+        let info = req.body;
+
+        let idEdit = req.params.id;
+
+        let producto = {
+            img: '',
+            nombre: info.nombre,
+            descripcion: info.descripcion,
+            anio: info.anio,
+            created_at: new DATE(),
+            updated_at: new DATE()
+        }
+
+        let filter = {
+            where : {
+                id: idEdit
+            }
+        }
+        
+        product.updateAdd(producto, filter)
+        .then((result) => {
+           return res.redirect("/");
+        }).catch((err) => {
+           return res.send(err)
+        }); 
+    }
+   
 }; 
 
 
---
+++ proyectoIntegradorG2/database/models/product.js
@@ -0,0 +1,46 @@
+module.exports = function (sequelize, dataTypes){
+    
+    let alias = 'Product';
+
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER
+        },
+        img: {
+            type: dataTypes.STRING
+        },
+        nombre: {
+            type: dataTypes.STRING
+        },
+        descripcion: {
+            type: dataTypes.STRING
+        },
+        anio: {
+            type: dataTypes.DATE,
+            allowNull: true,
+        },
+        comentarios: {
+            type: dataTypes.STRING
+        },
+        createdAt: {
+            type: dataTypes.DATE,
+            allowNull: true,
+        },
+        updatedAt: {
+            type: dataTypes.DATE,
+            allowNull: true,
+        },
+    }
+
+    let config = {
+        tableName: "products",
+        timestamps: true, 
+        underscored: false,
+    };
+
+    const Product = sequelize.define(alias, cols, config);
+
+    return Product;
+}
