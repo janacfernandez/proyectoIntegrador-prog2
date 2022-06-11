d8349bc8228ee2ae1e19b2c4c8f2adb5e3fc2646
Jana Fernandez <jcfernandez2@udesa.edu.ar>
2022-06-10 23:40:05 -0300

    avances index y ajustes detail/add/edit/update

--- proyectoIntegradorG2/controllers/indexcontroller.js
+++ proyectoIntegradorG2/controllers/indexcontroller.js
@@ -1,9 +1,19 @@
 const autos = require('../db/data');
+const db = require("../database/models"); //requiero db
+const product = db.Product;
+const bcrypt = require('bcryptjs');
+const op = db.Sequelize.Op;
+
 const controller = {
-    index: (req, res) => res.render('index', {
-           listaAutos: autos.productos
-       }),
-    searchResults: (req, res) => {
+    index: (req, res) => {
+      product
+      .findAll()
+      .then((result) => {
+        res.send(result)
+       /*return res.render("index", { listaAutos: result}); */
+    });
+}, 
+  searchResults: (req, res) => {
         let resultado = req.query.search;
         let autosEncontrados = [];
         for (let i = 0; i < autos.productos.length; i++) {
--- proyectoIntegradorG2/routes/products.js
+++ proyectoIntegradorG2/routes/products.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController'); 
 
-router.get('/detail/:id', productController.detail),
+router.get('/id/:id', productController.detail),
 
-router.get('/add', productController.add),
-router.post('/add', productController.procesarAdd),
+router.get('/add/', productController.add),
+router.post('/add/', productController.procesarAdd),
+
+router.get('/product-edit/:id', productController.edit),
+router.post('/product-edit/:id', productController.updateAdd), 
+
+router.get('/deleteProduct/:id', productController.destroy)
 
 
 module.exports = router;
