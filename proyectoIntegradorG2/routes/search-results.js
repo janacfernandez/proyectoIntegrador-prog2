b0366c185571483585653ecb5bd2d487ad07c31f
solanabiblos <sbiblos@udesa.edu.ar>
2022-06-17 16:47:56 -0300

    buscador terminado

--- proyectoIntegradorG2/routes/search-results.js
+++
@@ -1,9 +0,0 @@
-/* const express = require('express');
-const router = express.Router();
-const searchResultsController = require('../controllers/searchResultsController');
-
-
-
-router.get('/', searchResultsController.findProduct);
-
-module.exports = router; */
