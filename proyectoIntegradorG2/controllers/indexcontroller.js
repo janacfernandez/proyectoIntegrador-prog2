f4f7042666e02c995211d6182de12a1692ad0454
janacfernandez <jcfernandez@udesa.edu.ar>
2022-06-18 01:29:03 -0300

    arreglos varios

--- proyectoIntegradorG2/controllers/indexcontroller.js
+++ proyectoIntegradorG2/controllers/indexcontroller.js
@@ -6,42 +6,43 @@ const op = db.Sequelize.Op;
 
 const controller = {
     index: (req, res) => {
-      product.findAll({ 
+        product.findAll({
             order: [
-                ["createdAt" , "DESC"]
-            ], 
-            include: { 
-                all: true, 
-                nested: true }  
-    })
-      .then((result) => {
-       return res.render("index", {listaAutos: result}); 
-    }).catch((err) => {
-        console.log(err);
-    });     
-}, 
-/*hay que agregar los filtros */
-  searchResults: (req, res) => {
-        let resultado = req.query.search;
-        let filter ={
-            where :{
-                [op.or]: [
-                  { nombre: { [op.like]: `%${resultado}%` } },
-                  { descripcion: { [op.like]: `%${resultado}%` } }
-                ]
-              }, 
-               include: [ { association: 'user' }]
+                ["createdAt", "DESC"]
+            ],
+            include: {
+                all: true,
+                nested: true
             }
-      
-            product.findAll(filter)
+        })
             .then((result) => {
-            return res.render('search-results', { listaAutos: result, resultado: req.query.search} )
+                return res.render("index", { listaAutos: result });
             }).catch((err) => {
                 console.log(err);
             });
+    },
+    /*hay que agregar los filtros */
+    searchResults: (req, res) => {
+        let resultado = req.query.search;
+        let filter = {
+            where: {
+                [op.or]: [
+                    { nombre: { [op.like]: `%${resultado}%` } },
+                    { descripcion: { [op.like]: `%${resultado}%` } }
+                ]
+            },
+            include: [{ association: 'user' }]
         }
-    
-    };
+
+        product.findAll(filter)
+            .then((result) => {
+                return res.render('search-results', { listaAutos: result, resultado: req.query.search })
+            }).catch((err) => {
+                console.log(err);
+            });
+    }
+
+};
 
 
 
--- proyectoIntegradorG2/controllers/productController.js
+++ proyectoIntegradorG2/controllers/productController.js
@@ -5,7 +5,8 @@ const Product = db.Product;
 const controller = {
     detail: (req, res) => {
         let id = req.params.id;
-        Product.findByPk(id).then((result)=>{
+        Product.findByPk(id).then((result) => {
+            console.log(result)
             let product = {
                 id: result.dataValues.id,
                 img: result.dataValues.img,
@@ -13,19 +14,23 @@ const controller = {
                 descripcion: result.descripcion,
                 anio: result.anio,
                 createdAt: new Date(),
-                updatedAt: new Date()
-            }
-            return res.render('product-detail', {
-                listaAutos: product})
-        })
-        .catch(err => console.log(err));
-        },
+                userId: result.userId,
 
-    add: (req, res) =>  res.render('product-add'),
+            }
+            console.log(product.userId.usuario)
+            return res.render('product-detail', {
+                listaAutos: product,
+                usuario: product.userId, 
+            })
+        })
+            .catch(err => console.log(err));
+    },
+
+    add: (req, res) => res.render('product-add'),
 
     store: (req, res) => {
         let info = req.body;
-        let imgProductAdd = req.file.filename; 
+        let imgProductAdd = req.file.filename;
         let product = {
             img: imgProductAdd,
             nombre: info.nombre,
@@ -37,39 +42,40 @@ const controller = {
         }
 
         Product.create(product)
-        .then((result) => {
-            return res.redirect("/")
-          })
-        .catch(err => res.send("Hay un error" + err));
-        }, 
-        
+            .then((result) => {
+                return res.redirect("/")
+            })
+            .catch(err => res.send("Hay un error" + err));
+    },
+
     edit: (req, res) => {
         let id = req.params.id;
 
         Product.findByPk(id)
-        .then((result) => {
-            let product = {
-                id: result.dataValues.id, 
-                img: result.dataValues.img,
-                nombre: result.dataValues.nombre,
-                descripcion: result.dataValues.descripcion,
-                anio: result.dataValues.anio,
-            }
-            return res.render('product-edit', {
-                listaAutos: product})
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+            .then((result) => {
+                let product = {
+                    id: result.dataValues.id,
+                    img: result.dataValues.img,
+                    nombre: result.dataValues.nombre,
+                    descripcion: result.dataValues.descripcion,
+                    anio: result.dataValues.anio,
+                }
+                return res.render('product-edit', {
+                    listaAutos: product
+                })
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     },
 
-    updateProd : (req, res) => {
+    updateProd: (req, res) => {
         let info = req.body;
-        let imgProductEdit = req.file.filename; 
+        let imgProductEdit = req.file.filename;
         let idEdit = req.params.id;
 
         let producto = {
-            img:imgProductEdit,
+            img: imgProductEdit,
             nombre: info.nombre,
             descripcion: info.descripcion,
             anio: info.anio,
@@ -78,31 +84,31 @@ const controller = {
         }
 
         let filter = {
-            where : {
+            where: {
                 id: idEdit
             }
         }
-        
+
         Product.update(producto, filter)
-        .then((result) => {
-           return res.redirect("/");
-        }).catch((err) => {
-           return res.send(err)
-        }); 
-    }, 
+            .then((result) => {
+                return res.redirect("/");
+            }).catch((err) => {
+                return res.send(err)
+            });
+    },
     delete: (req, res) => {
         let idDelete = req.params.id;
         Product.destroy({
-            where : {
-                id : idDelete
+            where: {
+                id: idDelete
             }
         })
-        .then((result) => {
-            console.log(result);
-            return res.redirect('/')
-        })
+            .then((result) => {
+                console.log(result);
+                return res.redirect('/')
+            })
     }
-}; 
+};
 module.exports = controller;
 
 //  comentarios: (req, res) => {
@@ -114,28 +120,28 @@ module.exports = controller;
        //     ['created_at', 'DESC']
       //  ]
 
-    /*   procesarComentarios : (req, res) => {
-        let info = req.body
+/*   procesarComentarios : (req, res) => {
+    let info = req.body
+    let comentario = {
+        comentario : info.comentario
+    }
+    Comentario.create(comentario)
+    .then((result) => {
+        return res.redirect('/products/')
+    }).catch((err) => {
+        console.log(err);
+    });
+},
+showComentarios : (req, res) => {
+    let id = req.params.id;
+    Comentario.findByPk(id).then((result) => {
         let comentario = {
-            comentario : info.comentario
+            comentario: result.comentario, 
         }
-        Comentario.create(comentario)
-        .then((result) => {
-            return res.redirect('/products/')
-        }).catch((err) => {
-            console.log(err);
-        });
-    },
-    showComentarios : (req, res) => {
-        let id = req.params.id;
-        Comentario.findByPk(id).then((result) => {
-            let comentario = {
-                comentario: result.comentario, 
-            }
-        return res.render('products', {comentarios : comentario})
-        }).catch((err) => {
-            console.log(err);
-        });
-    },
+    return res.render('products', {comentarios : comentario})
+    }).catch((err) => {
+        console.log(err);
+    });
+},
 
-     */
+ */
